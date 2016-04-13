4ab3a573d8186e6751ed851adedebbe3160b6048
nicolas acard <acardnicolas@hotmail.com>
2016-04-14 00:32:45 +0200

    show card

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,12 +5,13 @@ class PagesController < ApplicationController
 
   def home
     ad = Ad.count
-    @ads = Ad.all.sort[ad-9..ad].reverse
+    #@ads = Ad.all.sort[ad-9..ad].reverse
+    @ads = Ad.order(id: :desc).limit(9)
 
   #si la date du vol est supérieur à la date.now on incremente un conmpteur de 1 dans une boucle
   @ads_counter = 0
   Ad.all.each do |c|
-    if c.flight_date > DateTime.now
+    if c.flight_date >= DateTime.now
       @ads_counter = @ads_counter + 1
     end
   end
